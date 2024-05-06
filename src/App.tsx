64705b37867de154885e560ffc2d14cddfb913e8
Sarah Bealing <smb272@uclive.ac.nz>
2024-05-06 17:17:24 +1200

    Clean up styling. Add logic for adding items to cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import './App.css';
 
-function App() {
-    const productText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+const PRODUCT_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+const PRODUCT_NAME = "Classic Tee";
+const PRODUCT_PRICE = "$75.00";
 
-    //variable for cart contents
+const sizes = ["S", "M", "L"] as const;
+type Size = typeof sizes[number];
+
+type CartItem = {
+    name: string,
+    price: string,
+    size: Size
+}
+
+function App() {
+    const [cart, setCart] = useState<CartItem[]>([]); //Cart display will group items by size i.e quantity
+    const [selectedSize, setSelectedSize] = useState<Size | null>(null);
+    const [errorText, setErrorText] = useState<string>('');
+
+    const addToCart = () => {
+        if (selectedSize === null) {
+            setErrorText("Please select a size");
+        } else {
+            setCart(cart.concat([{
+                    name: PRODUCT_NAME,
+                    price: PRODUCT_PRICE,
+                    size: selectedSize
+                }]));
+        }
+    }
 
     //TODO: call product API for info: title, desc, price, size options
-    //replace divs with styled tags e.g. <Title>, <Price> etc
 
   return (
     <div>
-      <div className="header">My Cart ( 4 )</div>
+      <div className="header">My Cart ( {cart.length} )</div>
       <div className="body">
-        <div className="image">[Image here]</div>
+        <img src="classic-tee.jpg" alt="Model wearing Classic Tee"/>
         <div className="details">
-            <div className="title">Classic Tee</div>
-            <div className="price">$75.00</div>
-            <div className="description">{productText}</div>
+            <div className="title">{PRODUCT_NAME}</div>
+            <div className="price">{PRODUCT_PRICE}</div>
+            <div className="description">{PRODUCT_TEXT}</div>
             <div>
-                <div className="size">SIZE*</div>
+                <div className="size">SIZE<span className="required">*</span>
+                <span className="selectedSize"> {selectedSize ?? errorText}</span></div>
                 <div>
-                    <Button variant="outlined">S</Button>
-                    <Button variant="outlined">M</Button>
-                    <Button variant="outlined">L</Button>
+                    {sizes.map((size) => (
+                        <Button
+                            sx={size === selectedSize
+                            ? {
+                                color: '#222222',
+                                borderColor: '#222222',
+                                marginRight: '5px'
+                            } : {
+                                color: '#888888',
+                                borderColor: '#CCCCCC',
+                                marginRight: '5px'
+                            }}
+                            key={size}
+                            onClick={() => setSelectedSize(size)}
+                            variant="outlined">
+                            {size}
+                        </Button>
+                    ))}
                 </div>
-                <Button variant="outlined">ADD TO CART</Button>
+                <Button sx={{color: '#222222', borderColor: '#222222', marginTop: '10px'}}
+                    variant="outlined"
+                    onClick={() => addToCart()}
+                >
+                    ADD TO CART
+                </Button>
             </div>
         </div>
       </div>
