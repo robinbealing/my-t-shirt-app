3faed55c10cdf092d62ffab3cfefa319d2527204
Sarah Bealing <smb272@uclive.ac.nz>
2024-05-06 15:52:27 +1200

    Import MUI. Basic element layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from '@mui/material/Button';
 import './App.css';
 
 function App() {
+    const productText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+
+    //variable for cart contents
+
+    //TODO: call product API for info: title, desc, price, size options
+    //replace divs with styled tags e.g. <Title>, <Price> etc
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">My Cart ( 4 )</div>
+      <div className="body">
+        <div className="image">[Image here]</div>
+        <div className="details">
+            <div className="title">Classic Tee</div>
+            <div className="price">$75.00</div>
+            <div className="description">{productText}</div>
+            <div>
+                <div className="size">SIZE*</div>
+                <div>
+                    <Button variant="outlined">S</Button>
+                    <Button variant="outlined">M</Button>
+                    <Button variant="outlined">L</Button>
+                </div>
+                <Button variant="outlined">ADD TO CART</Button>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
