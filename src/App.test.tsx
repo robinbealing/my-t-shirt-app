731fb1daa46f8554d4c868ff22809485c5115ac6
Sarah Bealing <smb272@uclive.ac.nz>
2024-05-06 17:52:44 +1200

    Add tests for existing functionality

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('clicking Add to Cart when no size selected shows error', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const errorText = screen.queryByText("Please select a size");
+  expect(errorText).not.toBeInDocument;
+  const addButton = screen.getByRole('button', {name: "ADD TO CART"});
+  fireEvent.click(addButton);
+  screen.getByText("Please select a size");
+});
+
+test('clicking Add to Cart when size selected adds to cart count', () => {
+  render(<App />);
+  screen.getByText("My Cart ( 0 )");
+  const smallButton = screen.getByRole('button', {name: "S"});
+  const addButton = screen.getByRole('button', {name: "ADD TO CART"});
+  fireEvent.click(smallButton);
+  fireEvent.click(addButton);
+  screen.getByText("My Cart ( 1 )");
 });
